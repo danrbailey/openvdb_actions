d150da22632521b6bbaeaf5f9b8ff589bf087d3d
Dan Bailey <danbailey@ilm.com>
2020-02-19 23:58:06 -0800

    wip

--- ci/install_houdini.sh
+++ ci/install_houdini.sh
@@ -4,19 +4,26 @@ set -e
 
 HOUDINI_MAJOR="$1"
 GOLD="$2"
-HOUDINI_CLIENT_ID="$3"
-HOUDINI_SECRET_KEY="$4"
+REQUIRE_SECRETS="$3"
+HOUDINI_CLIENT_ID="$4"
+HOUDINI_SECRET_KEY="$5"
 
 if [ "$HOUDINI_CLIENT_ID" == "" ]; then
     echo "HOUDINI_CLIENT_ID GitHub Action Secret needs to be set to install Houdini builds"
+    if [ "$REQUIRE_SECRETS" == "ON" ]; then
+        exit 1
+    fi
 fi
 if [ "$HOUDINI_SECRET_KEY" == "" ]; then
     echo "HOUDINI_SECRET_KEY GitHub Action Secret needs to be set to install Houdini builds"
+    if [ "$REQUIRE_SECRETS" == "ON" ]; then
+        exit 1
+    fi
 fi
 
 pip install --user requests
 
-python ci/download_houdini2.py $HOUDINI_MAJOR $GOLD $HOUDINI_CLIENT_ID $HOUDINI_SECRET_KEY
+python ci/download_houdini.py $HOUDINI_MAJOR $GOLD $HOUDINI_CLIENT_ID $HOUDINI_SECRET_KEY
 
 tar -xzf hou.tar.gz
 ln -s houdini* hou
